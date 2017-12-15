febebd63594756798c2feb7fb6ac7a3637796b17
SporadicToast <mgc.macaraeg@gmail.com>
2017-12-15 22:50:55 +0800

    Login 2

--- SilverSteel_IMS/SilverSteel_IMS/Form1.Designer.vb
+++ SilverSteel_IMS/SilverSteel_IMS/Form1.Designer.vb
@@ -27,6 +27,7 @@ Partial Class form_login
         Me.Label2 = New System.Windows.Forms.Label()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'lbl_top
@@ -70,11 +71,21 @@ Partial Class form_login
         Me.TextBox2.Size = New System.Drawing.Size(168, 20)
         Me.TextBox2.TabIndex = 1
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(310, 58)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'form_login
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(425, 101)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label2)
@@ -92,4 +103,5 @@ Partial Class form_login
     Friend WithEvents Label2 As Label
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
 End Class
