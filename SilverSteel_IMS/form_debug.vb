b2d4a1783492467e3aacf3932ecd81c6c1c11b33
SporadicToast <mgc.macaraeg@gmail.com>
2018-01-01 20:10:51 +0800

    Commit Login Menu to Login Branch

---
+++ SilverSteel_IMS/form_debug.vb
@@ -0,0 +1,19 @@
+﻿Public Class form_debug
+    Private Sub btn_debug_login_Click(sender As Object, e As EventArgs) Handles btn_debug_login.Click
+        form_login.Show()
+    End Sub
+
+    Private Sub btn_debug_menu_Click(sender As Object, e As EventArgs) Handles btn_debug_menu.Click
+        mainmenu.Show()
+    End Sub
+
+    Private Sub btn_debug_var_Click(sender As Object, e As EventArgs) Handles btn_debug_var.Click
+        If debug = True Then
+            lbl_debugvar.Text = "FALSE"
+            debug = False
+        Else
+            lbl_debugvar.Text = "TRUE"
+            debug = True
+        End If
+    End Sub
+End Class
---
+++ SilverSteel_IMS/mainprocess.vb
@@ -0,0 +1,23 @@
+﻿Module mainprocess
+    'Variable Declarations
+    'Debug Variable
+    Public debug As Boolean
+    Public version As String
+    'Close Function
+
+    'Get Passwords and Usernames
+    Public Function fetch_user() As String()
+        Dim user_string() As String
+        user_string = {"admin", "user"}
+        fetch_user = user_string
+    End Function
+    Public Function fetch_pass() As String()
+        Dim pass_string() As String
+        pass_string = {"admin", "user"}
+        fetch_pass = pass_string
+    End Function
+
+    Public Function close()
+        Application.Exit()
+    End Function
+End Module
