8c7b971e9795054e851732dafbb786b0125c85d7
Marc Macaraeg <mgc.macaraeg@gmail.com>
2018-01-26 11:28:23 +0800

    Add files via upload

---
+++ SilverSteel-main_features/SilverSteel_IMS/form_login.vb
@@ -0,0 +1,53 @@
+﻿Public Class form_login
+    Dim auth_success As Boolean
+    Private Sub btn_exit_Click(sender As Object, e As EventArgs) Handles btn_exit.Click
+        'Exit Application
+        mainprocess.close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Check if Bypass Login is enabled
+        If cb_debug_bypass.Checked = False Then
+            'Check username
+            If fetch_user.Contains(tb_user.Text) Then
+                'Check password
+                If fetch_pass.Contains(tb_pass.Text) Then
+                    'Check credentials
+                    If Array.IndexOf(fetch_user, tb_user.Text) = Array.IndexOf(fetch_pass, tb_pass.Text) Then
+                        auth_success = True
+                    Else
+                        auth_success = False
+                    End If
+                Else
+                    auth_success = False
+                End If
+            Else
+                auth_success = False
+            End If
+        Else
+            auth_success = True
+        End If
+
+        If auth_success = True Then
+            'Success prompt
+            MsgBox("Successfully logged in.")
+            DATEandTIMEmainmenuIMS.Show()
+            If cb_debug_window.Checked Then
+                mainprocess.debug = True
+                form_debug.Show()
+            Else
+                mainprocess.debug = False
+            End If
+            Me.Close()
+        Else
+            'Fail prompt
+            MsgBox("Credentials failed!")
+        End If
+    End Sub
+
+    'Login
+
+    'Exit Application
+
+
+End Class
---
+++ SilverSteel-main_features/SilverSteel_IMS/mainprocess.vb
@@ -0,0 +1,67 @@
+﻿Imports System.Data.OleDb
+Module mainprocess
+    'Variable Declarations
+    'Debug Variable
+    Public debug As Boolean
+    Public version As String
+    Public imsdb As New OleDbConnection
+    'Carwash Variables
+    Public carwash_bool() As Boolean = {False, False, False, False, False, False}
+    'Debug Print
+    Public Function debug_print(message As String) As String
+        System.Diagnostics.Debug.WriteLine(message)
+    End Function
+    'Connection String
+    Public Function load_connection() As String
+
+        imsdb.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=|DataDirectory|\ss_ims.mdb"
+        Try
+            imsdb.Open()
+        Catch ex As Exception
+            debug_print("Opening failed.")
+            MsgBox("Connection to the database has failed, make sure you are using this in a x86 system, database connection errors will occur.")
+        End Try
+        debug_print("Connection attempt completed")
+    End Function
+    'Carwash Datalist
+    ' 0    1    2    3    4    5    6    7    8    9    10
+    'start end lp  card. cart. clnt a.p. s.t. l.c. com. vac.
+    'declare as (x)(y) where x is carwash descriptor and y carwash slot
+    Public carwash_datalist(,) As String = {{"", "", "", "", "", "", "", "", "", "", ""}, {"", "", "", "", "", "", "", "", "", "", ""}, {"", "", "", "", "", "", "", "", "", "", ""}, {"", "", "", "", "", "", "", "", "", "", ""}, {"", "", "", "", "", "", "", "", "", "", ""}, {"", "", "", "", "", "", "", "", "", "", ""}}
+
+
+
+    'Get Passwords and Usernames
+    Public Function fetch_user() As String()
+        Dim user_string() As String
+        user_string = {"admin", "user"}
+        fetch_user = user_string
+    End Function
+    Public Function fetch_pass() As String()
+        Dim pass_string() As String
+        pass_string = {"admin", "user"}
+        fetch_pass = pass_string
+    End Function
+
+    'Automatic Off Add Screen On Add
+    Public Function add_lock(form As String, sel_ind As Integer) As String
+        If form = "carwash" And carwash_bool(sel_ind) = False Then
+            form_c_service.gb_carwash.Enabled = True
+        End If
+        If form = "carwash" And carwash_bool(sel_ind) = True Then
+            form_c_service.gb_carwash.Enabled = False
+        End If
+    End Function
+
+    'Carwash Time Handler
+
+    'Lock Application
+    Public Function lock()
+        form_login.Show()
+        DATEandTIMEmainmenuIMS.Close()
+    End Function
+    'Close Application
+    Public Function close()
+        Application.Exit()
+    End Function
+End Module
